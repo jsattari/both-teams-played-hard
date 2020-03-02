ee379661354a01ab8f12e1842e1b9c6716069af6
John Sattari <johnsattari@Johns-MacBook-Air.local>
2020-03-01 16:55:52 -0800

    fixed bokeh glyph. Added hovertool, slider (not complete)

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -4,68 +4,66 @@ from bokeh.io import output_file, show, curdoc
 from bokeh.models import ColumnDataSource, HoverTool
 from bokeh.models.widgets import Select, Slider
 from bokeh.plotting import figure
-from bokeh.layouts import row, column
+from bokeh.transform import factor_cmap
+from bokeh.layouts import widgetbox, row
 
 # path to data file
 data = pd.read_csv(
     os.path.dirname(os.getcwd()) + '/Versus/data/merged_games.csv', index_col=False
 )
 
-data = data[['PLAYER_NAME', 'PTS', 'FGA', 'GAME_DATE_EST']]
+# Col_Names:
+# 'TEAM_ABBREVIATION', 'TEAM_CITY', 'PLAYER_ID', 'PLAYER_NAME',
+# 'START_POSITION', 'MIN', 'FGM', 'FGA', 'FG_PCT', 'FG3M', 'FG3A',
+# 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT', 'OREB', 'DREB', 'REB', 'AST', 'STL',
+# 'BLK', 'TO', 'PF', 'PTS', 'PLUS_MINUS', 'GAME_DATE_EST', 'COURT'
 
-data['GAME_DATE_EST'] = pd.to_datetime(data['GAME_DATE_EST'])
+# selecting which columns to include in dataset
+filtered = data[['PLAYER_NAME', 'PTS', 'FGA', 'GAME_DATE_EST', 'FGM', 'COURT']]
 
-data['GAME_DATE_EST'] = data['GAME_DATE_EST'].dt.strftime('%Y')
+# formatting 'game_date_est' col for datetime, changing to show only year
+filtered['GAME_DATE_EST'] = pd.to_datetime(filtered['GAME_DATE_EST'], format='%Y-%m-%d')
+filtered['GAME_DATE_EST'] = filtered['GAME_DATE_EST'].dt.year
 
-# data = data.groupby('PLAYER_NAME')
-# group the data and classify it for bokeh viz
-# grouped_data = data.groupby('PLAYER_NAME').agg(
-#    {'PTS': ['min', 'max', 'mean', 'var', 'median'], 'FGA': 'sum','GAME_DATE_EST':['min', 'max']})
-source = ColumnDataSource(data={
-    'x': data.loc[2003].PTS,
-    'y': data.loc[2003].FGA,
-    'player': data.loc[2003].PLAYER_NAME
-    })
+# fill blanks with 0s
+filtered.fillna(0)
 
-# determining plot attributes
+# declare source data for glyphs
+source = ColumnDataSource(filtered[filtered['GAME_DATE_EST'] == 2003])
+
+# coloring
+# index_cmap = factor_cmap(
+#     'COURT', palette=['red', 'blue', 'yellow'],
+#     factors=sorted(filtered['COURT'].unique()), end=1)
+
+# determining figure attributes
 p = figure(
-    plot_width=700, plot_height=500, toolbar_location="right", title="Scoring Efficiency"
-)
+    plot_width=700, plot_height=500,
+    toolbar_location="right", title="Scoring Efficiency by Totals",
+    title_location='above')
 
 # setting axis
 p.square(
-    x='x', y='y', source=source, size=5
-)
+    x='PTS', y='FGA', size=5, source=source)
 
 # determining hovertool 'hovers'
 hover_tool = HoverTool(
     tooltips=[
-        ('player_name', '@player')]
-)
+        ('Player', '@PLAYER_NAME'),
+        ('Total_Points', '@PTS'),
+        ('Total_FGM', '@FGM'),
+        ('Total_FGA', '@FGA'),
+        ('Location','@COURT')])
 
-# determining slider 
+# creating slider
 slider = Slider(
-    start=2003, end=2020, value=2003, step=1, title="Year"
-)
-
-# Define a callback function: callback
-def callback(attr, old, new):
-    yr = slider.value
-    new_data = {
-        'x': data.loc[yr].PTS,
-        'y': data.loc[yr].FGA,
-        'player': data.loc[yr].PLAYER_NAME
-    }
- 
-    # Update source with the new data values
-    source.data = new_data
-
-slider.on_change('value', callback)
+    start=filtered['GAME_DATE_EST'].min(),
+    end=filtered['GAME_DATE_EST'].max(),
+    value=filtered['GAME_DATE_EST'].min(), step=1, title="Season")
 
 # adding hovertool to glyph
 p.add_tools(hover_tool)
 
-layout = column(slider, p)
-curdoc().add_root(layout)
 
-show(layout)
+show(slider)
+show(p)
