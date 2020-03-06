68b298bc5dc85784f9f0d91d538159bfd918f426
John Sattari <johnsattari@Johns-MacBook-Air.local>
2020-03-05 21:18:44 -0800

    formatted file with if __name__

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -7,78 +7,87 @@ from bokeh.plotting import figure
 from bokeh.transform import factor_cmap
 from bokeh.layouts import row
 
-# path to data file
-data = pd.read_csv(
-    os.path.dirname(os.getcwd()) + '/Versus/data/merged_games.csv', index_col=False
-)
 
-# Col_Names:
-# 'TEAM_ABBREVIATION', 'TEAM_CITY', 'PLAYER_ID', 'PLAYER_NAME',
-# 'START_POSITION', 'MIN', 'FGM', 'FGA', 'FG_PCT', 'FG3M', 'FG3A',
-# 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT', 'OREB', 'DREB', 'REB', 'AST', 'STL',
-# 'BLK', 'TO', 'PF', 'PTS', 'PLUS_MINUS', 'GAME_DATE_EST', 'COURT'
+def main():
+    # path to data file
+    data = pd.read_csv(
+        os.path.dirname(os.getcwd()) + '/Versus/data/merged_games.csv', index_col=False
+    )
 
-# selecting which columns to include in dataset
-filtered = data[['PLAYER_NAME', 'PTS', 'FGA', 'GAME_DATE_EST', 'FGM', 'COURT']]
+    # Col_Names:
+    # 'TEAM_ABBREVIATION', 'TEAM_CITY', 'PLAYER_ID', 'PLAYER_NAME',
+    # 'START_POSITION', 'MIN', 'FGM', 'FGA', 'FG_PCT', 'FG3M', 'FG3A',
+    # 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT', 'OREB', 'DREB', 'REB', 'AST', 'STL',
+    # 'BLK', 'TO', 'PF', 'PTS', 'PLUS_MINUS', 'GAME_DATE_EST', 'COURT'
 
-# formatting 'game_date_est' col for datetime, changing to show only year
-filtered['GAME_DATE_EST'] = pd.to_datetime(filtered['GAME_DATE_EST'], format='%Y-%m-%d')
-filtered['GAME_DATE_EST'] = filtered['GAME_DATE_EST'].dt.year
+    # selecting which columns to include in dataset
+    filtered = data[['PLAYER_NAME', 'PTS', 'FGA', 'GAME_DATE_EST', 'FGM', 'COURT']]
 
-# fill blanks with 0s
-filtered.fillna(0)
+    # formatting 'game_date_est' col for datetime, changing to show only year
+    filtered['GAME_DATE_EST'] = pd.to_datetime(
+        filtered['GAME_DATE_EST'], format='%Y-%m-%d')
+    filtered['GAME_DATE_EST'] = filtered['GAME_DATE_EST'].dt.year
 
-# declare source data for glyphs
-source = ColumnDataSource(filtered[filtered['GAME_DATE_EST'] == 2003])
+    # fill blanks with 0s
+    filtered.fillna(0)
 
-# coloring
-# index_cmap = factor_cmap(
-#     'COURT', palette=['red', 'blue', 'yellow'],
-#     factors=sorted(filtered['COURT'].unique()), end=1)
+    # declare source data for glyphs
+    source = ColumnDataSource(filtered[filtered['GAME_DATE_EST'] == 2003])
 
-# determining figure attributes
-p = figure(
-    plot_width=700, plot_height=500,
-    toolbar_location="above", title="Scoring Efficiency by Totals",
-    title_location='above')
+    # coloring
+    # index_cmap = factor_cmap(
+    #     'COURT', palette=['red', 'blue', 'yellow'],
+    #     factors=sorted(filtered['COURT'].unique()), end=1)
 
-p.title.text_font_size = '25px'
-p.title.text_color = 'red'
-p.xaxis.axis_label = 'Total_Points'
-p.yaxis.axis_label = 'Total_Field_Goal_Attempts'
+    # determining figure attributes
+    p = figure(
+        plot_width=700, plot_height=500,
+        toolbar_location="above", title="Scoring Efficiency by Totals",
+        title_location='above')
 
-# setting axis
-p.square(
-    x='PTS', y='FGA', size=5, source=source)
+    p.title.text_font_size = '25px'
+    p.title.text_color = 'red'
+    p.xaxis.axis_label = 'Total_Points'
+    p.yaxis.axis_label = 'Total_Field_Goal_Attempts'
 
-# determining hovertool 'hovers'
-hover_tool = HoverTool(
-    tooltips=[
-        ('Player', '@PLAYER_NAME'),
-        ('Total_Points', '@PTS'),
-        ('Total_FGM', '@FGM'),
-        ('Total_FGA', '@FGA'),
-        ('Location','@COURT')])
+    # setting axis
+    p.square(
+        x='PTS', y='FGA', size=5, source=source)
 
-# adding hovertool to glyph
-p.add_tools(hover_tool)
+    # determining hovertool 'hovers'
+    hover_tool = HoverTool(
+        tooltips=[
+            ('Player', '@PLAYER_NAME'),
+            ('Total_Points', '@PTS'),
+            ('Total_FGM', '@FGM'),
+            ('Total_FGA', '@FGA'),
+            ('Location', '@COURT')])
 
-# creating slider
-slider = Slider(
-    start=filtered['GAME_DATE_EST'].min(),
-    end=filtered['GAME_DATE_EST'].max(),
-    value=filtered['GAME_DATE_EST'].min(), step=1, title="Season")
+    # adding hovertool to glyph
+    p.add_tools(hover_tool)
 
-# slider call back function
-def slider_update(attr, old, new):
-    new_year = slider.value
-    new_data = ColumnDataSource(filtered[filtered['GAME_DATE_EST'] == new_year])
-    source.data = new_data.data
+    # creating slider
+    slider = Slider(
+        start=filtered['GAME_DATE_EST'].min(),
+        end=filtered['GAME_DATE_EST'].max(),
+        value=filtered['GAME_DATE_EST'].min(), step=1, title="Season")
+
+    # slider call back function
 
 
-slider.on_change('value', slider_update)
+    def slider_update(attr, old, new):
+        new_year = slider.value
+        new_data = ColumnDataSource(
+            filtered[filtered['GAME_DATE_EST'] == new_year])
+        source.data = new_data.data
 
-layout = row(
-    slider, p)
 
-show(layout)
+    slider.on_change('value', slider_update)
+
+    layout = row(
+        slider, p)
+
+    show(layout)
+
+if __name__ == "__main__":
+    main()
